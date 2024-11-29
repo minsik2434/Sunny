4541946f94b06a2c7b6cd0d11f91da1a4bc7fd21
minsik2434 <minsik2434@gmail.com>
2024-11-29 15:46:44 +0900

    Feat: Project Service init

---
+++ project-service/src/test/java/com/sunny/projectservice/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.projectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
